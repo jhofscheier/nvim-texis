86809e8efe37bdb47354cc0c2385cff081804bad
Johannes Hofscheier <johanneshofscheier@MBPvonJohannes.lan>
2022-07-11 10:25:23 +0100

    lua/nvimtexis/view.lua:
    (1) moved utility function `ends_with`, `bufid`, and winid into separate
        util-module
    (2) use vim.loop for neovim's file io support (instead of lua's io)
    (3) use new neovim 0.7+ api
    (4) refactored code to be better readable

--- lua/nvimtexis/view.lua
+++ lua/nvimtexis/view.lua
@@ -4,68 +4,48 @@
 local view = {}
 
 local cache = require('nvimtexis.cache')
+local util = require('nvimtexis.util')
+local uv = vim.loop
 
-local function ends_with(str, ending)
-	return ending == "" or str:sub(-#ending) == ending
-end
-
-local function is_buffer_loaded(file)
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if ends_with(vim.api.nvim_buf_get_name(buf), file) then
-			return buf
-		end
-	end
-	return -1
-end
-
-local function winid_of_buffer(buffer)
-	for _, winid in ipairs(vim.api.nvim_list_wins()) do
-		if vim.api.nvim_win_get_buf(winid) == buffer then
-			return winid
-		end
-	end
-	return -1
-end
-
-function view.inverse_search(line, file_name)
+function view.inverse_search(filename, line)
 	-- resolve possible symlinks
-	local file = vim.api.nvim_call_function('resolve', {file_name})
+	local file = uv.fs_realpath(filename)
+	local buf, win, tab, ok
+
+	-- is file already loaded into a buffer?
+	ok, buf = pcall(util.bufid, file)
+	if not ok then
+		-- open file if necessary
+		ok, buf = pcall(util.open, file)
+		if not ok then
+			return false
+		end
+	end
+
+	-- line within range?
+	if (line > vim.api.nvim_buf_line_count(buf)) or (line < 1) then
+		return false
+	end
+
+	-- default to `normal mode`
 	if vim.api.nvim_get_mode()['mode'] == 'i' then
 		vim.cmd('stopinsert')
 	end
 
-	local bufnr = is_buffer_loaded(file)
-	if buf == -1 then
-		if vim.api.nvim_call_function('filereadable', {file}) == 1 then
-			local status_ok, _ = pcall(vim.cmd,
-									   cache.nvimtexis_vise_cmd .. ' ' .. file)
-			if not status_ok then
-				print("Reverse goto failed; command error: ",
-					  cache.nvimtexis_vise_cmd, file)
-				return -3
-			end
-			bufnr = vim.api.nvim_get_current_buf()
-		else
-			print("Reverse goto failed; file not readable: '", file, "'")
-			return -4
-		end
+	-- get buffer, window, and tab handles
+	ok, win = pcall(util.winid, buf)
+	if ok then
+		tab = vim.api.nvim_win_get_tabpage(win)
+		-- If tab/window exists, switch to it/them
+		vim.api.nvim_set_current_tabpage(tab)
+		vim.api.nvim_set_current_win(win)
+	else -- if no window for buffer use current window
+		vim.api.nvim_set_current_buf(buf)
+		win = 0 -- use current window
 	end
 
-	-- Get buffer, window, and tab numbers
-	-- * If tab/window exists, switch to it/them
-	local winid = winid_of_buffer(bufnr)
-	if winid >= 0 then
-		-- do we have to use vim.api.nvim_tabpage_get_number(tabnr)?
-		local tabnr = vim.api.nvim_win_get_tabpage(winid)
-		local winnr = vim.api.nvim_win_get_number(winid)
-		vim.cmd(tostring(tabnr) .. 'tabnext')
-		vim.cmd(tostring(winnr) .. 'wincmd w')
-	else -- if more or no window views are open for buffer
-		vim.cmd(nvimtexis_vise_cmd .. ' ' .. file)
-	end
-
-	vim.cmd('normal!' .. tostring(line) .. 'G')
-	vim.cmd('redraw')
+	vim.api.nvim_win_set_cursor(win, { line, 0 })
+	return true
 end
 
 return view
