4d57b7bf28d9792ef4bf2e1ba6d6544177173ec9
Johannes Hofscheier <johannes.hofscheier@gmail.com>
2022-07-17 21:39:18 +0100

    updated to use new API

--- lua/nvimtexis/init.lua
+++ lua/nvimtexis/init.lua
@@ -3,7 +3,7 @@ if not vim.g.nvimtexis_enabled then
 	return
 end
 
-local cache = require "nvimtexis.cache"
+local cache = require("nvimtexis.cache")
 
 -- prune servernames and save to file
-cache.prune_servernames()
+cache:prune_servernames()
