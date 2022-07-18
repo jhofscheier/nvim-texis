8f94f32869a0cb29af3b4aa368582bf35d822f21
Johannes Hofscheier <johannes.hofscheier@gmail.com>
2022-07-18 10:06:20 +0100

    ftplugin/tex.lua no longer needed

--- ftplugin/tex.lua
+++
@@ -1,6 +0,0 @@
--- No 'exists(b:did_ftplugin)'. Load nvimtexis alongside other tex-plugins.
--- If vim.g.nvimtexis_enabled isn't set to 1 nothing will be imported.
-if vim.g.nvimtexis_enabled then
-	-- require nvimtexis to update server list (only once per neovim instance)
-	require('nvimtexis')
-end
