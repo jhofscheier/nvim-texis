308c1bba269454810ee444ac09edaa2ca08baa9d
Johannes Hofscheier <johanneshofscheier@MBPvonJohannes2.lan>
2023-07-19 22:29:45 +0100

    refactor(caller): revision for easier inverse search
    - simplified approach how to pass arguments from the pdf viewer to
      neovim.

--- lua/nvimtexis/caller.lua
+++ lua/nvimtexis/caller.lua
@@ -1,32 +1,40 @@
 -- Functions for pdf-viewers to communicate with neovim
 
-local caller = {}
-
 local cache = require('nvimtexis.cache')
 
-function caller.inverse_search(filename, line)
-	if line > 0 and filename and filename ~= '' then
-		local ok, servers, socket
-		ok, servers = pcall(cache.servernames, cache)
-		-- if opening servernames file failed then exit
-		if not ok then
-			vim.cmd('quitall!')
-		end
-		for server in servers:gmatch("(.-)\n") do
-			ok, socket = pcall(vim.api.nvim_call_function,
-							   'sockconnect',
-							   {'pipe', server, {rpc = 1}})
-			if ok then
-				vim.rpcrequest(
-					socket,
-					'nvim_exec_lua',
-					[[return require('nvimtexis.view').inverse_search(...)]],
-					{
-						filename,
-						line,
-					}
-				)
-				vim.api.nvim_call_function('chanclose', {socket})
+local caller = {}
+
+---Sends an rpc request setting the line number in a particular file in all
+---active Neovim instances. Line number and filename are extracted from `args`.
+---@param args string # expected format 'filename' 'line number' where quotes
+---                     can be left out, single or double quotes
+function caller.inverse_search(args)
+	---@type string, string
+	local _, filename, _, line = args:match([[(["']?)(.+)%1 (["']?)(%d+)%3]])
+	if filename ~= '' and line ~= '' then
+		line = tonumber(line)
+		if line > 0 and filename and filename ~= '' then
+			---@type boolean, string, integer
+			local ok, servers, socket
+			ok, servers = pcall(cache.servernames)
+			-- if opening servernames file failed then exit
+			if not ok then
+				vim.cmd('quitall!')
+			end
+			for server in servers:gmatch("(.-)\n")  do
+				---@type boolean, integer
+				ok, socket = pcall(vim.api.nvim_call_function,
+								   'sockconnect',
+								   {'pipe', server, {rpc = 1}})
+				if ok then
+
+					vim.rpcrequest(
+					   socket,
+					   'nvim_exec_lua',
+					   [[return require('nvimtexis.view').inverse_search(...)]],
+					   { filename, line, })
+					vim.api.nvim_call_function('chanclose', {socket})
+				end
 			end
 		end
 	end
