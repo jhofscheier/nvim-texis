05cf7fc3261bc55397ef1328936955900e5c78fd
Johannes Hofscheier <johanneshofscheier@MBPvonJohannes2.lan>
2023-07-19 22:32:52 +0100

    refactor(init): update to reflect recent changes

--- lua/nvimtexis/init.lua
+++ lua/nvimtexis/init.lua
@@ -1,9 +1,14 @@
--- Only load plugin if enabled
-if not vim.g.nvimtexis_enabled then
-	return
+local cache = require("nvimtexis.cache")
+local Config = require("nvimtexis.config")
+
+local M = {}
+
+---Sets the plugin up by overwriting default settings with user's configuration
+---and creates autocommands for cache management.
+---@param user_opts NvimTeXis.Config?
+function M.setup(user_opts)
+	Config.setup(user_opts)
+	cache.autocmd_servernames()
 end
 
-local cache = require("nvimtexis.cache")
-
--- prune servernames and save to file
-cache:prune_servernames()
+return M
